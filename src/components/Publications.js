305ea6327bc77af67d0f496676d48ff2705f0b83
lucasvelez78 <lucasvelezv@gmail.com>
2023-02-08 15:52:53 -0500

    deploy setup

--- src/components/Publications.js
+++ src/components/Publications.js
@@ -6,11 +6,12 @@ function Publications({ posts }) {
           return (
             <div className="post">
               <video preload="auto" width={120} height={100} controls>
-                <source src={`http://localhost:3001${post.videos[0]}`} />
+                <source
+                  src={`https://blog-vvm-server.onrender.com${post.videos[0]}`}
+                />
               </video>
               <div className="post-description">
                 <h6>{post.name}</h6>
-                {/* <p>{post.content}</p> */}
               </div>
             </div>
           );
